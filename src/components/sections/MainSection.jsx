9888b54e3c3e0b64b2374137b228351606448b9e
Mohak Waghchaure <mohak11jan@gmail.com>
2024-11-25 18:59:59 -0500

    updates

--- src/components/sections/MainSection.jsx
+++ src/components/sections/MainSection.jsx
@@ -35,13 +35,13 @@ export default function MainSection({selectedNavOption}) {
         <MapComponent></MapComponent>
       </div>
       <div className="chart-wrapper" id="stackedBarChart">
-        <div className="title">Interactive US Map with State-Wise Population Data</div>
-        <div className="description">Explore an interactive map showcasing U.S. state population estimates from 2020 to 2023! With features like zoom, click, hover, and pan, you can effortlessly navigate and compare state-wise demographic trends. Powered by reliable and up-to-date datasets, this tool provides a clear and insightful view of population changes across the country over recent years.</div>
+        <div className="title">Stacked Bar Chart</div>
+        {/* <div className="description">Explore an interactive map showcasing U.S. state population estimates from 2020 to 2023! With features like zoom, click, hover, and pan, you can effortlessly navigate and compare state-wise demographic trends. Powered by reliable and up-to-date datasets, this tool provides a clear and insightful view of population changes across the country over recent years.</div> */}
         <StackedBarChart></StackedBarChart>
       </div>
       <div className="chart-wrapper" id="stackedAreaChart">
-        <div className="title">Interactive US Map with State-Wise Population Data</div>
-        <div className="description">Explore an interactive map showcasing U.S. state population estimates from 2020 to 2023! With features like zoom, click, hover, and pan, you can effortlessly navigate and compare state-wise demographic trends. Powered by reliable and up-to-date datasets, this tool provides a clear and insightful view of population changes across the country over recent years.</div>
+        <div className="title">Stacked Area Chart</div>
+        {/* <div className="description">Explore an interactive map showcasing U.S. state population estimates from 2020 to 2023! With features like zoom, click, hover, and pan, you can effortlessly navigate and compare state-wise demographic trends. Powered by reliable and up-to-date datasets, this tool provides a clear and insightful view of population changes across the country over recent years.</div> */}
         <StackedAreaChart></StackedAreaChart>
       </div>
       <div className="chart-wrapper" id="wordCloud">
